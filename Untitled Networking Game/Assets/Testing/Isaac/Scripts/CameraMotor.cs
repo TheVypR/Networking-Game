a1e7c8d58519b8145f595a216e3b605596293fd5
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-01-26 14:46:06 -0500

    Camera Smoothing and Automove

--- Untitled Networking Game/Assets/Testing/Isaac/Scripts/CameraMotor.cs
+++ Untitled Networking Game/Assets/Testing/Isaac/Scripts/CameraMotor.cs
@@ -10,6 +10,7 @@ public class CameraMotor : MonoBehaviour
     public float moveSpeed = 0.1f;
     public float autoSpeed = 0f;
     public float followDistance = 3f;
+    Vector3 offset = new Vector3(0, 5, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +21,28 @@ public class CameraMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_playerTrans.position.y > transform.position.y + followDistance)
+        Follow();
+        AutoMove();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPos = transform.position;
+
+        if(_playerTrans.position.y > transform.position.y + 2)
         {
-            transform.position += new Vector3(0, moveSpeed, 0);
-        } else if(_playerTrans.position.y < transform.position.y - followDistance)
+            targetPos = new Vector3(transform.position.x, _playerTrans.position.y, 0) + offset;
+        } else if(_playerTrans.position.y < transform.position.y - 2)
         {
-            transform.position -= new Vector3(0, moveSpeed, 0);
+            targetPos = new Vector3(transform.position.x, _playerTrans.position.y, -10) - offset;
         }
+
+        Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, moveSpeed);
+        transform.position = smoothPos;
+    }
+
+    void AutoMove()
+    {
+        transform.position += new Vector3(autoSpeed, 0, 0);
     }
 }
