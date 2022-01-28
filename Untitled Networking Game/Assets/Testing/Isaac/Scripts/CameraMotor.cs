28345e130fe855a778357da46138617e7bb45dd2
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-01-28 14:14:55 -0500

    Added a debug mode on camera that follows on x-axis

--- Untitled Networking Game/Assets/Testing/Isaac/Scripts/CameraMotor.cs
+++ Untitled Networking Game/Assets/Testing/Isaac/Scripts/CameraMotor.cs
@@ -6,11 +6,15 @@ public class CameraMotor : MonoBehaviour
 {
     public Transform _playerTrans;
 
+    //debug var
+    public bool debug;
+
     //control vars
     public float moveSpeed = 0.1f;
     public float autoSpeed = 0f;
     public float followDistance = 3f;
-    Vector3 offset = new Vector3(0, 5, 0);
+    Vector3 Yoffset = new Vector3(0, 5, 0);
+    Vector3 Xoffset = new Vector3(5, 0, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -21,24 +25,52 @@ public class CameraMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Follow();
-        AutoMove();
+        if (!debug)
+        {
+            AutoMove();
+        }
+        Follow(debug);
     }
 
-    void Follow()
+    void Follow(bool isDebug)
     {
         Vector3 targetPos = transform.position;
 
-        if(_playerTrans.position.y > transform.position.y + 2)
+        if (isDebug)
         {
-            targetPos = new Vector3(transform.position.x, _playerTrans.position.y, 0) + offset;
-        } else if(_playerTrans.position.y < transform.position.y - 2)
-        {
-            targetPos = new Vector3(transform.position.x, _playerTrans.position.y, -10) - offset;
-        }
+            if (_playerTrans.position.y > transform.position.y + 2)
+            {
+                targetPos = new Vector3(transform.position.x, _playerTrans.position.y, 0) + Yoffset;
+            }
+            else if (_playerTrans.position.y < transform.position.y - 2)
+            {
+                targetPos = new Vector3(transform.position.x, _playerTrans.position.y, -10) - Yoffset;
+            }
+            else if(_playerTrans.position.x > transform.position.x + 3)
+            {
+                targetPos = new Vector3(_playerTrans.position.x, transform.position.y, -10) + Xoffset;
+            }
+            else if(_playerTrans.position.x < transform.position.x - 3)
+            {
+                targetPos = new Vector3(_playerTrans.position.x, transform.position.y, -10) - Xoffset;
+            }
 
-        Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, moveSpeed);
-        transform.position = smoothPos;
+            Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, moveSpeed);
+            transform.position = smoothPos;
+        } else
+        {
+            if (_playerTrans.position.y > transform.position.y + 2)
+            {
+                targetPos = new Vector3(transform.position.x, _playerTrans.position.y, 0) + Yoffset;
+            }
+            else if (_playerTrans.position.y < transform.position.y - 2)
+            {
+                targetPos = new Vector3(transform.position.x, _playerTrans.position.y, -10) - Yoffset;
+            }
+
+            Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, moveSpeed);
+            transform.position = smoothPos;
+        }        
     }
 
     void AutoMove()
