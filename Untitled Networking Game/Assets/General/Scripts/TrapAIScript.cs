0f775c65bf75eca554d71fdf99bdcdb73c5e5d11
Enoch Andreades <57719394+Helethel@users.noreply.github.com>
2022-01-31 12:11:08 -0500

    Start of trap ai

---
+++ Untitled Networking Game/Assets/General/Scripts/TrapAIScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapAIScript : MonoBehaviour
+{
+
+    public GameObject spawners;
+    public GameObject mine;
+    System.Random rando = new System.Random();
+    BombStrikeScript bombScript;
+    LavaScript lavaScript;
+    EconomyScript econScript;
+    private int MAX_TRAP_COST = 75;
+    public GameObject bombSPrefab;
+    public GameObject lavaPrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (econScript.money > MAX_TRAP_COST)
+        {
+            int num = rando.Next(0, 5000);
+
+            if (num < 50)
+            {
+                Instantiate(bombSPrefab);
+                econScript.SpendCoin(bombScript._cost);
+            }
+            else if (num < 10)
+            {
+                Instantiate(lavaPrefab);
+                econScript.SpendCoin(lavaScript.cost);
+            }
+        }
+    }
+}
