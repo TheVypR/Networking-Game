98358eb0a569b8412afb0ed3bab195688c4c8b04
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-02-01 13:11:31 -0500

    Bombing animation

--- Untitled Networking Game/Assets/BombScript.cs
+++ Untitled Networking Game/Assets/BombScript.cs
@@ -8,6 +8,7 @@ public class BombScript : MonoBehaviour
     CircleCollider2D _cCol;
     SpriteRenderer _rend;
     Rigidbody2D _rBody;
+    Animator _anim;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class BombScript : MonoBehaviour
         _cCol = GetComponent<CircleCollider2D>();
         _rend = GetComponent<SpriteRenderer>();
         _rBody = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,7 +32,7 @@ public class BombScript : MonoBehaviour
         _cCol.enabled = false;
         _rend.enabled = false;
         _rBody.gravityScale = 0;
-        Invoke("DestroyBomb", 0.5f);
+        _anim.Play("Explosion");
     }
 
     void DestroyBomb()
