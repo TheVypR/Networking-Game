4b1c459661fbac368e8b150abb3035c1d24c3d53
TheVypR <57635954+TheVypR@users.noreply.github.com>
2022-02-11 12:39:32 -0500

    Minor finish

--- Untitled Networking Game/Assets/FinishScript.cs
+++ Untitled Networking Game/Assets/FinishScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishScript : MonoBehaviour
 {
@@ -27,6 +28,6 @@ public class FinishScript : MonoBehaviour
 
     void EndGame()
     {
-
+        SceneManager.LoadScene("LevelSelectScene");
     }
 }
