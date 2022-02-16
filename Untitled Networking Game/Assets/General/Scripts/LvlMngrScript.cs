7bb850ba34b81df5c2e582cf5a2c836845788bf3
MaxPargeon <70291262+MaxPargeon@users.noreply.github.com>
2022-02-16 08:58:16 -0500

    Added Respawn Canvas

--- Untitled Networking Game/Assets/General/Scripts/LvlMngrScript.cs
+++ Untitled Networking Game/Assets/General/Scripts/LvlMngrScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System;
 
 public class LvlMngrScript : MonoBehaviour
 {
@@ -26,12 +28,21 @@ public class LvlMngrScript : MonoBehaviour
     public Text _winScreen;
     public Text _loseScreen;
 
+
+    //Respawn Screen
+    public TMP_Text _respawning;
+    public Text _respawnText;
+    public Text _respawnTextBack;
+    public float _timeRespawn;
+    public bool _dead = false;
+
     // Start is called before the first frame update
     void Start()
     {
         _playerRbody = _player.GetComponent<Rigidbody2D>();
         _playerTrans = _player.GetComponent<Transform>();
         startTime = Time.time;
+        _timeRespawn = 4f;
     }
 
     // Update is called once per frame
@@ -54,6 +65,21 @@ public class LvlMngrScript : MonoBehaviour
             GameOver(2);
         }
 
+        if (_dead)
+        {
+            _respawning.enabled = true;
+            _respawnText.enabled = true;
+            _respawnTextBack.enabled = true;
+            if (_timeRespawn > 0)
+            {
+                _timeRespawn -= Time.deltaTime;
+                _respawnText.text = "" + _timeRespawn;
+                _respawnTextBack.text = "" + _timeRespawn;
+
+                Debug.Log(_timeRespawn);
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.K))
         {
             Instantiate(_airStrike);
@@ -67,6 +93,16 @@ public class LvlMngrScript : MonoBehaviour
 
     void Respawn()
     {
+        //Handle all respawn timer and texts
+        {
+            _respawning.enabled = false;
+            _respawnText.enabled = false;
+            _respawnTextBack.enabled = false;
+            _timeRespawn = 4f;
+            _dead = false;
+        }
+
+
         _player.SetActive(true);
 
         _playerTrans.position = spwn;
@@ -80,7 +116,9 @@ public class LvlMngrScript : MonoBehaviour
     {
         this.spwn = spwn;
         _player.SetActive(false);
-        Invoke("Respawn", 2);
+
+        _dead = true;
+        Invoke("Respawn", 3);
     }
 
     public void GameOver(int player)
@@ -101,5 +139,10 @@ public class LvlMngrScript : MonoBehaviour
     void EndGame()
     {
         SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void RespawnTime()
+    {
+        
     }
 }
