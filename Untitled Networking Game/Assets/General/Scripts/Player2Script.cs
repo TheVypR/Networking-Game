f042199ca00833273d69db5500b17e329461b420
Enoch Andreades <57719394+Helethel@users.noreply.github.com>
2022-03-05 15:10:57 -0500

    Preliminary Player2 controls

--- Untitled Networking Game/Assets/General/Scripts/Player2Script.cs
+++ Untitled Networking Game/Assets/General/Scripts/Player2Script.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Player2Script : MonoBehaviour
 {
+    bool player2 = false;
+    public GameObject spawners;
+    float x;
+    int spawns;
+    int place = 0;
+    Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawns = spawners.transform.childCount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (player2)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (place >= spawns)
+                {
+                    spawns = 0;
+                }
+                else
+                {
+                    place++;
+                }
+                target = spawners.transform.GetChild(place);
+            }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (place < 0)
+                {
+                    place = spawns - 1;
+                }
+                else
+                {
+                    place--;
+                }
+                target = spawners.transform.GetChild(place);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        gameObject.transform.position = target.position;
+
     }
 }
