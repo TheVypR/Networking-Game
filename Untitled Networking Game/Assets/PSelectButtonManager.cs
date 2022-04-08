f98945b476282499ff0aebb50d8c55c23734618b
MaxPargeon <70291262+MaxPargeon@users.noreply.github.com>
2022-04-08 15:52:40 -0400

    Start of button manager

---
+++ Untitled Networking Game/Assets/PSelectButtonManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSelectButtonManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RunnerSelect()
+    {
+        //player prefs set for player to be runner
+        print("You are now the runner!");
+
+        //TODO: Don't allow player to select anything else after selecting
+    }
+
+    public void TrapperSelect()
+    {
+        //player prefs set for player to be trapper
+        print("You are now the trapper!");
+
+        //TODO: Don't allow player to select anything else after selecting
+    }
+}
